0a83fc3e48a7a0b3aa86acbda366a3dfcd5f8b95
issubham <iamsubhamhaldar.dev@gmail.com>
2026-06-19 11:48:48 +0530

    fix: android button appearance override and bump sw version

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
     Update: bump CACHE_VERSION when deploying new code.
 ==========================================================*/
 
-const CACHE_VERSION  = 'v4';
+const CACHE_VERSION  = 'v5';
 const CACHE_NAME     = `daily-os-${CACHE_VERSION}`;
 
 const PRECACHE_URLS = [
